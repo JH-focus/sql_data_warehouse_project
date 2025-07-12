978562ac36657cd6f7fc6d7ff894071b2539a7e0
JH-focus <gauklerjeremias@yahoo.de>
2025-07-12 11:16:06 +0200

    Update bulk_insert_bronze_layer.sql

--- scripts/bronze_layer/bulk_insert_bronze_layer.sql
+++ scripts/bronze_layer/bulk_insert_bronze_layer.sql
@@ -9,3 +9,18 @@ This makes this method very fast but less flexible.
 TRUNCATE TABLE bronze.crm_cust_info;
 \COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/cust_info.csv' DELIMITER ',' HEADER CSV
 
+TRUNCATE TABLE bronze.crm_prod_info;
+\COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' DELIMITER ',' HEADER CSV
+
+TRUNCATE TABLE bronze.crm_sales_details;
+\COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' DELIMITER ',' HEADER CSV
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+\COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/CUST_AZ12.csv' DELIMITER ',' HEADER CSV
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+\COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/LOC_A101.csv' DELIMITER ',' HEADER CSV
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+\COPY bronze.crm_cust_info FROM 'C:/Users/gaukl/Downloads/sql-data-warehouse-project/datasets/source_crm/PX_CAT_G1V2.csv' DELIMITER ',' HEADER CSV
+
