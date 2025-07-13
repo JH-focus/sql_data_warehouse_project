9e1de1c42a3287c4555f77b6448756a3f8d72046
JH-focus <gauklerjeremias@yahoo.de>
2025-07-13 20:05:30 +0200

    Update crm_cust_info_cleanse.sql

--- scripts/silver_layer/crm_cust_info_cleanse.sql
+++ scripts/silver_layer/crm_cust_info_cleanse.sql
@@ -1,4 +1,18 @@
-SELECT * FROM (
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+ELSE 'Unknown'
+END AS cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'M'THEN 'Male'
+WHEN UPPER(TRIM(cst_gndr)) = 'F'THEN 'Female'
+ELSE 'Unknown'
+END AS cst_gndr,
+cst_create_date
+FROM (
 SELECT
 *,
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
