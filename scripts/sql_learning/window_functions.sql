3c47e292e78d6b42f9b5f3630417cebf1234f2d7
JH-focus <gauklerjeremias@yahoo.de>
2025-08-09 15:56:24 +0200

    Update window_functions.sql

--- scripts/sql_learning/window_functions.sql
+++ scripts/sql_learning/window_functions.sql
@@ -1,3 +1,4 @@
+--SUM() OVER (PARTITION BY ...)
 SELECT
 order_number,
 order_date,
@@ -7,10 +8,56 @@ FROM gold.fact_sales f;
 --Find the total sales for each product
 --additionally provide details such as order ID, order date
 SELECT
-order_number,
-order_date,
-product_key,
-sales_amount,
+f.order_number,
+f.order_date,
+f.product_key,
+f.sales_amount,
+p.category,
+p.subcategory,
 SUM(sales_amount) OVER() as total_sales,
-SUM(sales_amount) OVER (PARTITION BY product_key) AS total_sales_by_products
-FROM gold.fact_sales;
+SUM(sales_amount) OVER (PARTITION BY p.category) AS total_sales_by_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key;
+
+--RANK() OVER (PARTITION BY)
+SELECT
+f.order_date,
+f.order_number,
+f.sales_amount,
+p.category,
+p.subcategory,
+RANK() OVER (ORDER BY sales_amount ASC) AS rank_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key;
+
+--window frame definition
+--extremely useful when determining an average over the last three months, or years...
+--important to consider that at the end of the table -> the subset 
+SELECT
+f.order_date,
+f.order_number,
+f.sales_amount,
+p.category,
+p.subcategory,
+--the frame always has to be defined from "lowest to highest" -> meaning:
+--ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING
+--ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+--ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING
+ROUND(AVG(f.sales_amount) OVER (ORDER BY f.order_date ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING), 2) AS avg_test_sales,
+ROUND(SUM(f.sales_amount) OVER (ORDER BY f.order_date ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING), 2) AS avg_test_sum,
+ROUND(AVG(f.sales_amount) OVER (ORDER BY f.order_date ROWS BETWEEN 1 PRECEDING AND CURRENT ROW), 2) AS avg_test_sales,
+ROUND(SUM(f.sales_amount) OVER (PARTITION BY DATE_TRUNC('YEAR', f.order_date) ORDER BY f.order_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING), 2) AS entire_window,
+--Shortcut for preceding rows, works ONLY for preceding, not for following!
+ROUND(AVG(f.sales_amount) OVER (ORDER BY f.order_date ROWS 1 PRECEDING), 2) AS avg_test_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key;
+
+SELECT
+f.customer_key,
+SUM(f.sales_amount) AS total_sales,
+RANK() OVER (ORDER BY SUM(f.sales_amount) DESC)
+FROM gold.fact_sales f
+GROUP BY f.customer_key;
