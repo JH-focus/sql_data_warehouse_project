9d10f8098e1c418cc401508e5f895ed7b36479fe
JH-focus <gauklerjeremias@yahoo.de>
2025-08-10 16:01:23 +0200

    sql learning_window_functions
    further examples on window functions and their use cases

--- scripts/sql_learning/window_functions.sql
+++ scripts/sql_learning/window_functions.sql
@@ -55,9 +55,66 @@ FROM gold.fact_sales f
 LEFT JOIN gold.dim_products p
 ON p.product_key = f.product_key;
 
+--RANK() function
+--handles ties -> same rank possible with blank ranks, identical values will have the exact same rank
 SELECT
 f.customer_key,
 SUM(f.sales_amount) AS total_sales,
 RANK() OVER (ORDER BY SUM(f.sales_amount) DESC)
 FROM gold.fact_sales f
 GROUP BY f.customer_key;
+
+--ROW_NUMBER() function
+--not handling ties - same values will have different row number
+SELECT
+f.sales_amount,
+p.category,
+p.subcategory,
+ROW_NUMBER() OVER (PARTITION BY p.category ORDER BY f.sales_amount DESC) AS rn
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key;
+
+--Top-N analysis
+--Highest sales by product_key
+SELECT * 
+FROM(
+SELECT
+f.order_number,
+f.product_key,
+f.sales_amount,
+ROW_NUMBER() OVER (PARTITION BY f.product_key ORDER BY f.sales_amount DESC) rank_by_product,
+DENSE_RANK() OVER (PARTITION BY f.product_key ORDER BY f.sales_amount DESC) dense_rank_by_product
+FROM gold.fact_sales f
+)
+WHERE rank_by_product <= 5;
+
+--bottom-N analysis
+--lowest 2 total_sales by customers
+SELECT *
+FROM(
+SELECT
+f.customer_key,
+SUM(f.sales_amount),
+ROW_NUMBER() OVER (ORDER BY SUM(f.sales_amount) ASC) AS rank_customers
+FROM gold.fact_sales f
+GROUP BY f.customer_key
+)
+WHERE rank_customers <= 2;
+
+--assign unique IDs to rows of gold.fact_sales
+SELECT
+REPLACE('ID' || TO_CHAR(ROW_NUMBER() OVER (ORDER BY f.order_date), '00000'), ' ', '') AS unique_id,
+LENGTH(REPLACE('ID' || TO_CHAR(ROW_NUMBER() OVER (ORDER BY f.order_date), '00000'), ' ', '')) AS length_id,
+*
+FROM gold.fact_sales f;
+
+--clean duplicates using ROW_NUMBER()
+SELECT *
+FROM(
+SELECT
+f.order_number,
+ROW_NUMBER() OVER (PARTITION BY order_number ORDER BY order_date DESC) rn
+FROM gold.fact_sales f
+)
+WHERE rn = 1;
