33b47bd4b19847f1cd79de8099932606da835111
JH-focus <gauklerjeremias@yahoo.de>
2025-09-25 21:51:01 +0200

    Update stored_procedures.sql

--- scripts/sql_learning/stored_procedures.sql
+++ scripts/sql_learning/stored_procedures.sql
@@ -1,23 +1,30 @@
---Create a stored procedure for a customer summary
+--Create a stored procedure/function for a customer summary
 SELECT *
 FROM gold.report_customers;
 
-DROP FUNCTION gold.vip_customer_summary
+DROP FUNCTION gold.customer_summary;
 
-
-CREATE OR REPLACE PROCEDURE gold.vip_customer_summary()
-LANGUAGE plpgsql
+--define the function, together with the output table and the 
+--parameters, that you want to make optional, as well as a default value 
+CREATE OR REPLACE FUNCTION gold.customer_summary(p_segment text DEFAULT 'vip')
+RETURNS TABLE (
+  age_group VARCHAR(20),
+  customer_segment VARCHAR(20),
+  total_customers bigint,
+  total_sales numeric
+)
+LANGUAGE sql
 AS $$
-BEGIN
-	SELECT
-		age_group,
-		COUNT(*) AS total_customers,
-		SUM(total_sales) AS total_sales
-	FROM gold.report_customers
-	WHERE customer_segment = 'vip'
-	GROUP BY age_group;
-END;
+  SELECT
+    rc.age_group,
+	rc.customer_segment,
+    COUNT(*)::bigint         AS total_customers,
+    SUM(rc.total_sales)::numeric AS total_sales
+  FROM gold.report_customers rc
+  WHERE rc.customer_segment = p_segment
+  GROUP BY rc.age_group, rc.customer_segment;
 $$;
 
-
-CALL gold.vip_customer_summary();
+--this calls the function, providing the option to specify the parameter 
+--or use the default value, in this case 'vip'
+SELECT * FROM gold.customer_summary();
